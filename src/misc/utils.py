449f03861a5e0d81fc533e3edc1bde689b02b4a3
Nikita Shvetsov <nikita.shvetsov@uit.no>
2021-12-02 07:20:54 +0000

    Move exp_decay function

--- src/misc/utils.py
+++ src/misc/utils.py
@@ -7,6 +7,14 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+def exp_decay_lr_schedule(nr_epochs, init_lr, decay_factor, decay_steps):
+    lr_sched = []
+    for i in range(1, nr_epochs):
+        if i % decay_steps == 0:
+            decay = decay_factor ** (i / decay_steps)
+            new_lr = init_lr * decay
+            lr_sched.append((i, new_lr))
+    return lr_sched
 
 def show_best_checkpoint(path, compare_value="epoch_num", comparator="max"):
     with open(os.path.join(path, "stats.json"), "r") as read_file:
